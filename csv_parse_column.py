c1963a623931bc0e23d134d405415ff4f51947df
afpacket <andrewfox312@gmail.com>
2018-08-14 18:48:16 -0500

    move argparse to main, update gitignore

--- csv_parse_column.py
+++ csv_parse_column.py
@@ -3,16 +3,6 @@
 import csv
 from argparse import ArgumentParser
 
-parser = ArgumentParser()
-parser.add_argument("filename", type=str,
-                    help="csv filename to parse")
-parser.add_argument("-c", "--column", type=int, dest='column_number',
-                    default=0, help="the column number to parse (default=0)")
-args = parser.parse_args()
-
-csv_filename = args.filename
-column_number = args.column_number 
-
 def csv_parse(csv_file, column_number=0):
     """Read elements from specified column in CSV file and add them to list"""
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -25,6 +15,15 @@ def csv_parse(csv_file, column_number=0):
     return column_elements
 
 if __name__ == "__main__": # pronounced dunder main
+   parser = ArgumentParser()
+   parser.add_argument("filename", type=str,
+                       help="csv filename to parse")
+   parser.add_argument("-c", "--column", type=int, dest='column_number',
+                       default=0, help="the column number to parse (default=0)")
+   args = parser.parse_args()
+
+   csv_filename = args.filename
+   column_number = args.column_number
 
    with open(csv_filename) as csv_file:
         column = csv_parse(csv_file, column_number)
