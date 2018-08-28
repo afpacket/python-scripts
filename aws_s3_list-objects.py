0535f537066c96f15d1394459149117de48db437
Andrew Fox <andrewfox312@gmail.com>
2018-08-28 18:16:09 -0500

    spacing

--- aws_s3_list-objects.py
+++ aws_s3_list-objects.py
@@ -5,6 +5,7 @@ from argparse import ArgumentParser
 from botocore.exceptions import ClientError
 
 def s3_list_objects(client, s3_bucket_name):
+    """List objects in S3 Bucket"""
     try:
         for s3_object in client.list_objects_v2(Bucket=s3_bucket_name)['Contents']:
             print(s3_object['Key'])
@@ -15,8 +16,8 @@ def main():
    parser = ArgumentParser()
    parser.add_argument("bucket_name", type=str,
                        help="S3 bucket name")
-   args = parser.parse_args()
 
+   args = parser.parse_args()
    client = boto3.client('s3')
    s3_bucket_name = args.bucket_name
 
