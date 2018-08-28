63be4c885839e83813abab95aed7f28487662d52
Andrew Fox <andrewfox312@gmail.com>
2018-08-28 18:10:08 -0500

    simple python script to list objects in an S3 Bucket

---
+++ aws_s3_list-objects.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import boto3
+from argparse import ArgumentParser
+from botocore.exceptions import ClientError
+
+def s3_list_objects(client, s3_bucket_name):
+    try:
+        for s3_object in client.list_objects_v2(Bucket=s3_bucket_name)['Contents']:
+            print(s3_object['Key'])
+    except ClientError as error:
+           print(f"error: { error }")
+
+def main():
+   parser = ArgumentParser()
+   parser.add_argument("bucket_name", type=str,
+                       help="S3 bucket name")
+   args = parser.parse_args()
+
+   client = boto3.client('s3')
+   s3_bucket_name = args.bucket_name
+
+   s3_list_objects(client, s3_bucket_name)
+
+if __name__ == "__main__":
+   main()
